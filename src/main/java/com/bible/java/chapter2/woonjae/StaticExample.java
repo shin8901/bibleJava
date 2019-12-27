73623ccc26acd04772ca10aead1c3684197ccc62
WJ <WJ@DESKTOP-JCAC4M6>
2019-12-27 16:23:45 +0900

    앙 기모띠

---
+++ src/main/java/com/bible/java/chapter2/woonjae/StaticExample.java
@@ -0,0 +1,31 @@
+package com.bible.java.chapter2.woonjae;
+
+/**
+ * <pre>
+ * com.bible.java.chapter2
+ * VariableExample.java
+ *
+ * 클래스 개요 : xxx를 처리하는 클래스
+ * </pre>
+ */
+public class StaticExample {
+
+	/**
+	 * <pre>
+	 *   
+	 * 처리내용 : xxx를 처리하는 메소드
+	 * </pre>
+	 * 
+	 * @Method Name : main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Math.abs(1); // Java Lang 패키지에 있는 Math 클래스 에 abs 메소드
+		// 하지만 Math 클래스를 명시후에 abs 를 호출해야됨..
+		// 이렇게 하기가 시렁...
+
+		// Go to StaticExample2
+
+	}
+
+}
---
+++ src/main/java/com/bible/java/chapter2/woonjae/StaticExample2.java
@@ -0,0 +1,12 @@
+package com.bible.java.chapter2.woonjae;
+
+import static java.lang.Math.abs; // Import static 을 이용한다면
+
+public class StaticExample2 {
+
+	public static void main(String[] args) {
+		abs(1); // ??????????????????????
+
+	}
+
+}
