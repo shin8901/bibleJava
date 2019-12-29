2ca131f93246ec6c78bb388b8f382ace4a5b8bb0
WJ <WJ@DESKTOP-JCAC4M6>
2019-12-29 14:59:58 +0900

    끄적거리자

---
+++ src/main/java/com/bible/java/chapter3/woonjae/OperatorExample.java
@@ -0,0 +1,27 @@
+package com.bible.java.chapter3.woonjae;
+
+/**
+ * <pre>
+ * com.bible.java.chapter2
+ * VariableExample.java
+ *
+ * 클래스 개요 : xxx를 처리하는 클래스
+ * </pre>
+ */
+public class OperatorExample {
+
+	public static void main(String[] args) {
+
+		int i = 111;
+
+		System.out.println(i);
+		i = ~i;
+		System.out.println(i);
+
+		char a = 'a';
+		System.out.println(a);
+		System.out.println(~a);
+
+	}
+
+}
