1308d764919759a30fa2782c9c2cee0018ebc143
eunbi.woo <eunbi.woo@miracom.co.kr>
2020-01-02 13:08:16 +0900

    4, 5

---
+++ src/main/java/com/bible/java/chapter5/eunbi/Array.java
@@ -0,0 +1,35 @@
+package com.bible.java.chapter5.eunbi;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Array {
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5};
+		int[] b = new int[10];
+		b = a;
+		System.out.println(Arrays.toString(b));   //[1, 2, 3, 4, 5]  
+		int[] c = {1,2,3,4,5};
+		int[] d = new int[10];
+		for(int i = 0; i < c.length; i++) {
+			d[i] = c[i];
+		}
+		System.out.println(Arrays.toString(d)); //[1, 2, 3, 4, 5, 0, 0, 0, 0, 0]
+
+		int[] e = {1,2,3,4,5};
+		int[] f = new int[10];
+		
+		System.arraycopy(e, 0, f, 1, e.length);
+		
+		a[2] = 7;
+		c[2] = 7;
+		e[2] = 7;
+		System.out.println("A"+"_"+Arrays.toString(a));
+		System.out.println("B"+"_"+Arrays.toString(b));
+		System.out.println("C"+"_"+Arrays.toString(c));
+		System.out.println("D"+"_"+Arrays.toString(d));
+		System.out.println("E"+"_"+Arrays.toString(e));
+		System.out.println("F"+"_"+Arrays.toString(f));
+		
+	}
+}
