e243a8aeca5a939878cb94bc0dc18e7fcfeb2421
WJ <WJ@DESKTOP-JCAC4M6>
2020-01-06 17:06:18 +0900

    끄응

---
+++ src/main/java/com/bible/java/chapter6/woonjae/Constructor.java
@@ -0,0 +1,16 @@
+package com.bible.java.chapter6.woonjae;
+
+class Constructor {
+
+	Constructor(int a) {
+
+	}
+
+	public static void main(String[] args) {
+		// Constructor c = new Constructor();
+		// 명시적으로 생성자가 존재하는경우 기본 생성자 없..
+		
+		Constructor c = new Constructor(1);
+		
+	}
+}
