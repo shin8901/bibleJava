f1161e7a9166fcffebda3bde0c110c7f75997784
WJ <WJ@DESKTOP-JCAC4M6>
2020-01-06 17:15:16 +0900

    끄응

--- src/main/java/com/bible/java/chapter6/woonjae/InitializeBlock.java
+++ src/main/java/com/bible/java/chapter6/woonjae/InitializeBlock.java
@@ -1,6 +1,6 @@
 package com.bible.java.chapter6.woonjae;
 
-class InitializeBlock {
+public class InitializeBlock {
 
 	static {
 		// 클래스 초기화 블럭
@@ -15,7 +15,8 @@ class InitializeBlock {
 	public static void main(String[] args) {
 		// Constructor c = new Constructor();
 		// 명시적으로 생성자가 존재하는경우 기본 생성자 없..
-
+		
+		System.out.println("생성전");
 		InitializeBlock c = new InitializeBlock();
 
 	}
