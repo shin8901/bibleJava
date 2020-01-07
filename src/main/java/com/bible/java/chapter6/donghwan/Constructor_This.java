6ece4715c12cbde41f99bfecedf24f6113534ef7
DONGHWAN <DONGHWAN@DESKTOP-SHTCI1F>
2020-01-08 07:26:42 +0900

    1차 ㄱㄱ

---
+++ src/main/java/com/bible/java/chapter6/donghwan/Constructor_This.java
@@ -0,0 +1,34 @@
+package com.bible.java.chapter6.donghwan;
+
+class Car {
+	String color;
+	String gearType;
+	int door;
+
+	
+	Car(String color, String gearType, int door){
+		this.color = color;
+		this.gearType = gearType;
+		this.door = door;
+	}
+	
+	Car() {
+		this("white","auto",4);
+	}	
+	
+	Car(Car c){
+		color = c.color;
+		gearType = c.gearType;
+		door = c.door;
+	}
+}
+public class Constructor_This {
+
+	public static void main(String[] args) {
+		// 
+		Car c1 = new Car();
+		Car c2 = new Car(c1); //주소값을 받아 값을 복사한다 -> 독립적인 인스턴스를 생성한다
+
+	}
+
+}
---
+++ src/main/java/com/bible/java/chapter6/donghwan/ProductTest.java
@@ -0,0 +1,10 @@
+package com.bible.java.chapter6.donghwan;
+
+
+//멤버변수 초기화 시기와 순서
+public class ProductTest {
+
+	public static void main(String[] args) {
+
+	}
+}
