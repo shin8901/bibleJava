b6a2b0e293387dc315ba59b815a0c292da83c4eb
신운재 <woonjae.shin@miracom.co.kr>
2020-01-30 16:56:37 +0900

    삭제

--- src/main/java/com/bible/java/chapter10/woonjae/FormatTest.java
+++ src/main/java/com/bible/java/chapter10/woonjae/FormatTest.java
@@ -2,17 +2,10 @@ package com.bible.java.chapter10.woonjae;
 
 import java.text.ChoiceFormat;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.function.IntConsumer;
-
-import org.apache.commons.lang3.time.DateUtils;
 
 public class FormatTest {
 
 	public static void main(String[] args) {
-		// Calendar d = new Calendar();
-		// Calendar d = Calendar.getInstance();
 		test1();
 	}
 
@@ -33,25 +26,4 @@ public class FormatTest {
 		});
 	}
 
-	void test2() {
-
-		// 2.Date -> Calendar
-		Date d = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(d);
-	}
-
-	void test3() {
-
-		// 2.Date -> Calendar
-		Date now = new Date();
-		Date yesterday = DateUtils.addDays(now, -1);
-		Date tommorrow = DateUtils.addDays(now, 1);
-
-		System.out.println(now);
-		System.out.println(yesterday);
-		System.out.println(tommorrow);
-//		Calendar cal = Calendar.getInstance();
-//		cal.setTime(d);
-	}
 }
