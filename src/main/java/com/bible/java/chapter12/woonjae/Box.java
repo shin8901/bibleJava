2e15b8bd09478eac0f1adeb58b520bc2680826ff
shin8901 <snowman0107@naver.com>
2020-02-03 23:01:55 +0900

    멈추지마

---
+++ src/main/java/com/bible/java/chapter12/woonjae/Box.java
@@ -0,0 +1,14 @@
+package com.bible.java.chapter12.woonjae;
+
+public class Box<T> {
+
+	T item;
+
+	void setItem(T item) {
+		this.item = item;
+	}
+
+	T getItem() {
+		return item;
+	}
+}
---
+++ src/main/java/com/bible/java/chapter12/woonjae/BoxNotGenerics.java
@@ -0,0 +1,14 @@
+package com.bible.java.chapter12.woonjae;
+
+public class BoxNotGenerics {
+
+	Object item;
+
+	void setItem(Object item) {
+		this.item = item;
+	}
+
+	Object getItem() {
+		return item;
+	}
+}
