77c0af81361c081db6d4ab00aa2eaa12c9999390
shin8901 <snowman0107@naver.com>
2020-02-06 15:21:30 +0900

    까즈아ㅏㅏㅏㅏㅏㅏㅏㅏㅏ

--- src/main/java/com/bible/java/chapter12/woonjae/enumtest/Card.java
+++ src/main/java/com/bible/java/chapter12/woonjae/enumtest/Card.java
@@ -1,25 +1,35 @@
 package com.bible.java.chapter12.woonjae.enumtest;
 
-public class Card
-{
-    static final int CLOVER = 1;
-    static final int HEART = 2;
-    static final int DIAMOND = 3;
-    static final int SPACE = 4;
+public class Card {
+	static final int CLOVER = 0;
+	static final int HEART = 1;
+	static final int DIAMOND = 2;
+	static final int SPACE = 3;
 
-    final private int kind;
-    final private int num;
-    
+	static final int TWO = 0;
+	static final int THREE = 1;
+	static final int FOUR = 2;
+//	final int kind;
+//	final int num;
+
+	public static void main(String[] args) {
+		System.out.println(Card.CLOVER == Card.TWO);
+		
+		//System.out.println(CardEnum.Kind.CLOVER == CardEnum.Value.TWO);
+	}
+
 }
 
 class CardEnum {
-    enum Kind {        CLOVER,HEART,DIAMOND,SPACE} ;
-    
-    enum Value {
-        TWO,THREE,FOUR
-    };
+	enum Kind {
+		CLOVER, HEART, DIAMOND, SPACE
+	};
 
-    final Kind kind;
-    final Value value;
+	enum Value {
+		TWO, THREE, FOUR
+	};
+
+//	final Kind kind;
+//	final Value value;
 
 }
